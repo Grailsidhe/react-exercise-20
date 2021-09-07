12e71e76329b63ec4132706beb8f0ca7dc173f80
Lorianne Aguilar <78496780+Grailsidhe@users.noreply.github.com>
2021-09-07 22:48:41 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import React from "react";
+import Counter from "./components/Counter";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter title="Counter App" />
+    </div>
+  );
+}
---
+++ src/components/Counter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Counter extends React.Component {
+  render() {
+    return <h1>{this.props.title}</h1>;
+  }
+}
+
+export default Counter;
